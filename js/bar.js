08cf4b5fd0863978354b1b7ff92cdf684bf14a5d
byounger <byounger10@gmail.com>
2019-05-16 19:04:11 -0400

    Updated code to match against timescale code

--- js/bar.js
+++ js/bar.js
@@ -1,43 +1,53 @@
         
-        var margin = {
-            top: 15,
-            right: 25,
-            bottom: 15,
-            left: 60
-        };
+      // Set the dimensions of the canvas / graph
+    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+        width = 600 - margin.left - margin.right,
+        height = 270 - margin.top - margin.bottom;
 
-        var width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+    // Parse the date / time
+    var parseDate = d3.time.format("%Y-%m-%d %X").parse; //27-May-12 16:00:00. This is used for D3JS parsing
+ 
+    // Set the ranges
+    var x = d3.time.scale().range([height, 0]);
+    var y = d3.scale.ordinal().range([0, width]);
 
-	var parseTimestamp = d3.time.format("%Y-%m-%d %X").parse;
-	var formatTime = d3.time.format("%Y-%m-%d %X");
+    // Define the axes
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
 
-	var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-	var y = d3.time.scale.utc().range([height, 0]);
-        
-	var xAxis = d3.svg.axis()
-    	    .scale(x)
-            .orient("bottom");
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
 
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left")
-	    .ticks(24);
+    // Define the line
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.close); });
 
-	var svg = d3.select("#bar").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // Define 'div' for tooltips
+    var div = d3.select("body")
+        .append("div")  // declare the tooltip div
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
-	d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrNACrsMYkOo7cPMCLhGZqUKc4Dd9J663BU4i8Ml5aKlfaI2w64fFboGR_uQCAFmoUO0qNY7u2K0jj/pub?gid=2025866169&single=true&output=csv", function(error, data) {
-            data.forEach(function(d) {
-		d.Timestamp = parseTimestamp(d.Timestamp).format("YYYY-MM-DD HH:mm:ss");
-		d.FirstName = +d.FirstName;
-	    });
-	
-	x.domain(data.map(function(d) { return d.FirstName; }));
-  	y.domain(d3.extent(data, function(d) { return d.Timestamp; }));
+    // Adds the svg canvas
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrNACrsMYkOo7cPMCLhGZqUKc4Dd9J663BU4i8Ml5aKlfaI2w64fFboGR_uQCAFmoUO0qNY7u2K0jj/pub?gid=2025866169&single=true&output=csv", function(error, data) {
+        data.forEach(function(d) {
+            d.Timestamp = parseDate(moment.utc(d.Timestamp).format("YYYY-MM-DD HH:mm:ss"));
+            d.FirstName = +d.FirstName;
+        });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.FirstName; }));
+        y.domain([0, d3.max(data, function(d) { return d.TimeStamp; })]);
 		
 	 svg.append("g")
 	      .attr("class", "x axis")
