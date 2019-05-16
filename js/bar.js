62de0a192cb81e0ce9d805a63618e566c0845378
byounger <byounger10@gmail.com>
2019-05-15 23:19:10 -0400

    updated entire data append code

--- js/bar.js
+++ js/bar.js
@@ -1,7 +1,4 @@
         
-
-
-//set up svg using margin conventions - we'll need plenty of room on the left for labels
         var margin = {
             top: 15,
             right: 25,
@@ -12,72 +9,63 @@
         var width = 960 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
-        var svg = d3.select("#bar").append("svg")
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+	var y = d3.scale.linear().range([height, 0]);
+        
+	var xAxis = d3.svg.axis()
+    	    .scale(x)
+            .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10);
+
+	var svg = d3.select("#bar").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        
-d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRTE7B0FZzGglZwflximEKoDerxoJdfVmwdXyhLY_fWEbam0nmrLFz1qZTF4UvjsTio9GjhoyY9-Cf-/pub?gid=957248393&single=true&output=csv", function(a, b) {
-          return d3.ascending({ Timestamp: a.value, Age: b.value});
+	d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrNACrsMYkOo7cPMCLhGZqUKc4Dd9J663BU4i8Ml5aKlfaI2w64fFboGR_uQCAFmoUO0qNY7u2K0jj/pub?gid=2025866169&single=true&output=csv", function(error, data) {
+            data.forEach(function(d) {
+		d.Timestamp = +d.Timestamp;
+		d.FirstName = d.FirstName;
+	    });
 	
-	var timestamp = a.value;
+	x.domain(data.map(function(d) { return d.FirstName; }));
+  	y.domain([0, d3.max(data, function(d) { return d.Timestamp; })]);
+		
+	 svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis)
+	      .selectAll("text")
+	      .style("text-anchor", "end")
+	      .attr("dx", "-.8em")
+	      .attr("dy", "-.55em")
+	      .attr("transform", "rotate(-90)" );
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	      .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 5)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end");
+
+	  svg.selectAll("bar")
+	      .data(data)
+	      .enter().append("rect")
+	      .attr("class", "bar")
+	      .attr("x", function(d) { return x(d.FirstName); })
+	      .attr("width", x.rangeBand())
+	      .attr("y", function(d) { return y(d.Timestamp); })
+	      .attr("height", function(d) { return height - y(d.Timestamp); });
+
+	});
+
 	
-	var age = b.value;
-
-	var x = d3.scalelinear()
-            .range([0, width])
-            .domain([0, d3.max(data, function (d) {
-                return d.age;
-            })]);
-
-        var y = d3.scaleOrdinal()
-            .rangeRoundBands([height, 0], .1)
-            .domain(data.map(function (d) {
-                return d.timestamp;
-            }));
-
-        //make y axis to show bar names
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            //no tick marks
-            .tickSize(0)
-            .orient("left");
-
-        var gy = svg.append("g")
-            .attr("class", "x axis")
-            .call(xAxis);
-
-        var bars = svg.selectAll(".bar")
-            .data(data)
-            .enter()
-            .append("g");
-    
-        //append rects
-        bars.append("rect")
-            .attr("class", "bar")
-            .attr("x", function (d) {
-                return x(d.age);
-            })
-            .attr("height", x.rangeBand())
-            .attr("y", 0)
-            .attr("width", function (d) {
-                return y(d.timestamp);
-            });
-
-        //add a value label to the right of each bar
-        bars.append("text")
-            .attr("class", "label")
-            //y position of the label is halfway down the bar
-            .attr("x", function (d) {
-                return x(d.age) + x.rangeBand() / 2 + 4;
-            })
-            //x position is 3 pixels to the right of the bar
-            .attr("y", function (d) {
-                return y(d.age) + 3;
-            })
-            .text(function (d) {
-                return d.age;
-            });
-          });  
+	
